e020d94b34001d78b3c1de6e71ed0c25d2dfffb3
Sam Aryasa <sambya@aryasa.net>
2023-06-06 12:48:43 +0800

    Remove unused imports

--- src/Table/Table.stories.tsx
+++ src/Table/Table.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
-
-import Table, { ColumnDef } from "./Table";
+import "react";
+import Table from "./Table";
 
 const meta: Meta<typeof Table> = {
   title: "Example/Table",
